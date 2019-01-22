e1d810c709940aad619a71d8bb4fdcdbf122aa3a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-22 20:47:49 +0000

    Deploy Slashgear/webpack-workshop to github.com/Slashgear/webpack-workshop.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -21,7 +21,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "a17e98aeff3b9709b9f39b8f6971b092"
+    "revision": "cc3de5ae06e03f3961e31e95511dcc37"
   },
   {
     "url": "assets/css/0.styles.98e9d09f.css",
@@ -36,20 +36,28 @@ self.__precacheManifest = [
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/10.357d40c6.js",
-    "revision": "a385c45ae43e969a337745848b4be10d"
+    "url": "assets/js/10.2196e543.js",
+    "revision": "e9af2232cdcd83a9f17f757fb32f4869"
   },
   {
-    "url": "assets/js/11.5bcfe1b7.js",
-    "revision": "a5b13f92d1145ad037fc4670c5aaedab"
+    "url": "assets/js/11.0e8aa73a.js",
+    "revision": "3d21b3e0d75ba9fba8288474025858c0"
   },
   {
-    "url": "assets/js/2.67cf5634.js",
-    "revision": "49a5c34ff95438e5360e24658d039ddb"
+    "url": "assets/js/12.c2215e05.js",
+    "revision": "948ac4af0704fe55cd2b0d334b6fd371"
   },
   {
-    "url": "assets/js/3.5ed9f21a.js",
-    "revision": "2e473f2239200ff034495f039216f897"
+    "url": "assets/js/13.7004b56f.js",
+    "revision": "d5d7797d2432b32f87b3d09c38e018f5"
+  },
+  {
+    "url": "assets/js/2.83b44d2f.js",
+    "revision": "e43022a54e9006f030812fafd79af1d1"
+  },
+  {
+    "url": "assets/js/3.e91540fe.js",
+    "revision": "629f1822c9e42a4a13b12fcd702a5ecb"
   },
   {
     "url": "assets/js/4.7d56a664.js",
@@ -60,28 +68,28 @@ self.__precacheManifest = [
     "revision": "445c60016ca4a6bde41008e2130a7dc2"
   },
   {
-    "url": "assets/js/6.95d721b9.js",
-    "revision": "056331543f2413354efd53552c7f719b"
+    "url": "assets/js/6.5bc0512f.js",
+    "revision": "c64dd1833c8ada6daf7a12b3bcbf15f2"
   },
   {
-    "url": "assets/js/7.144835e1.js",
-    "revision": "8fe721ea5b64197acaf85a568613b5fd"
+    "url": "assets/js/7.fb4204c2.js",
+    "revision": "45980cfb826c5ff9a6748ef640fdd153"
   },
   {
-    "url": "assets/js/8.1ed9c1a6.js",
-    "revision": "6fa9b937380751303533347794a3d3cb"
+    "url": "assets/js/8.cd30e278.js",
+    "revision": "1bd8932b497e30a8975229a6282ba08c"
   },
   {
-    "url": "assets/js/9.b0dfb15a.js",
-    "revision": "4ed16d36bf7615bad4644fd5a64137dc"
+    "url": "assets/js/9.f88fca14.js",
+    "revision": "29d3dfc4bf130cbd0541ca464262f725"
   },
   {
-    "url": "assets/js/app.df404c31.js",
-    "revision": "c871cc27319052bfd4308066d7132d37"
+    "url": "assets/js/app.f48ed4de.js",
+    "revision": "5bc30454fcb3e8012983201726a36265"
   },
   {
     "url": "index.html",
-    "revision": "65c42e213d1ece6afcb0810d395ba7a1"
+    "revision": "30462f68b92369ae87a7a7a0669ef7db"
   },
   {
     "url": "webpack.png",
@@ -89,31 +97,39 @@ self.__precacheManifest = [
   },
   {
     "url": "workshops/index.html",
-    "revision": "0cb654507fcbacfecc2d2467061d7de0"
+    "revision": "f180f0730c2fe3ca4b0191e7798afaf1"
+  },
+  {
+    "url": "workshops/intermediate/dev.html",
+    "revision": "7cc07be0c26a93bdd699221804403d19"
+  },
+  {
+    "url": "workshops/intermediate/index.html",
+    "revision": "3ae3812cdaa447ac568ad3780a40df31"
   },
   {
     "url": "workshops/novice/basics.html",
-    "revision": "0d6e5aafa9fabe4592fdbcd350cf0c4b"
+    "revision": "84961453526b3b9ea2bc82c8a18374d9"
   },
   {
     "url": "workshops/novice/code-assets.html",
-    "revision": "d2e99b5d9b718dc1d9992654fc807415"
+    "revision": "d31892d1a0bc199e782ebf35e6848518"
   },
   {
     "url": "workshops/novice/index.html",
-    "revision": "fc2fabf875fdfec7ba9d1575f040ee53"
+    "revision": "6c785f0c9b987ed1806473d913d5ffaa"
   },
   {
     "url": "workshops/novice/novice-koans.html",
-    "revision": "8ee3c9552a7a26ad420f3f196136ca76"
+    "revision": "c8438d408d64c3c7b2c1d1c5b70952c7"
   },
   {
     "url": "workshops/novice/outputs.html",
-    "revision": "c243ae4d15de09a8339635329e5d07a5"
+    "revision": "73db87eb14b0d389baf392e214767311"
   },
   {
     "url": "workshops/novice/static-assets.html",
-    "revision": "4e2b824cf5305b12d004feacb3eeacde"
+    "revision": "35d78547a15b2a44775e637542b44d6b"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
